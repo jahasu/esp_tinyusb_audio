d23b116cf64e971c4edd3b1c9076f657c258f3bf
Jack Sutcliffe <jack.sutcliffe@manfrotto.com>
2022-11-30 11:57:23 +1300

    changed phy driveres

--- additions/src/tinyusb.c
+++ additions/src/tinyusb.c
@@ -19,7 +19,7 @@
 #include "esp_private/usb_phy.h"
 #include "soc/usb_pins.h"
 #include "tinyusb.h"
-// #include "descriptors_control.h"
+//#include "descriptors_control.h"
 #include "tusb.h"
 #include "tusb_tasks.h"
 
@@ -37,20 +37,20 @@ esp_err_t tinyusb_driver_install(const tinyusb_config_t *config)
         .controller = USB_PHY_CTRL_OTG,
         .otg_mode = USB_OTG_MODE_DEVICE,
     };
-    usb_phy_gpio_conf_t gpio_conf = {
-        .vp_io_num = USBPHY_VP_NUM,
-        .vm_io_num = USBPHY_VM_NUM,
-        .rcv_io_num = USBPHY_RCV_NUM,
-        .oen_io_num = USBPHY_OEN_NUM,
-        .vpo_io_num = USBPHY_VPO_NUM,
-        .vmo_io_num = USBPHY_VMO_NUM,
-    };
-    if (config->external_phy) {
-        phy_conf.target = USB_PHY_TARGET_EXT;
-        phy_conf.gpio_conf = &gpio_conf;
-    } else {
+    // usb_phy_gpio_conf_t gpio_conf = {
+    //     .vp_io_num = USBPHY_VP_NUM,
+    //     .vm_io_num = USBPHY_VM_NUM,
+    //     .rcv_io_num = USBPHY_RCV_NUM,
+    //     .oen_io_num = USBPHY_OEN_NUM,
+    //     .vpo_io_num = USBPHY_VPO_NUM,
+    //     .vmo_io_num = USBPHY_VMO_NUM,
+    // };
+    // if (config->external_phy) {
+    //     //phy_conf.target = USB_PHY_TARGET_EXT;
+    //     // phy_conf.gpio_conf = &gpio_conf;
+    // } else {
         phy_conf.target = USB_PHY_TARGET_INT;
-    }
+    //}
     ESP_RETURN_ON_ERROR(usb_new_phy(&phy_conf, &phy_hdl), TAG, "Install USB PHY failed");
 
     // dev_descriptor = config->descriptor ? config->descriptor : &descriptor_kconfig;
